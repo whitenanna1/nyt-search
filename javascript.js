76d0a5ca401da4d27b73f1249fbe5209a81f12cf
KHL562 <khl562sk@gmail.com>
2018-12-06 21:33:57 -0800

    fix

--- javascript.js
+++ javascript.js
@@ -1,37 +1,39 @@
-        $('#search').on('click', function() {
+$('#search').on('click', function () {
 
-        var searchTerm = $('#search').val();
-        // var searchTerm = 'george bush';
-        var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=83683e6f21874fe28ac4855fd25e2dbc&q=' + searchTerm + '&apikey=83683e6f21874fe28ac4855fd25e2dbc';
+    var searchTerm = $('#search').val();
+    // var searchTerm = 'george bush';
+    var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=83683e6f21874fe28ac4855fd25e2dbc&q=' + searchTerm + '&apikey=83683e6f21874fe28ac4855fd25e2dbc';
 
-        var numSelected = $('#numSelected').val();
+    var numSelected = $('#numSelected').val();
 
-        $.ajax({
+    $.ajax({
         url: queryURL,
         method: 'GET',
-        }).done(function(result) {
+    }).done(function (result) {
         console.log(result);
-        }).fail(function(err) {
+    }).fail(function (err) {
         throw err;
-        }).then(function(result) {
-            
-            for (var i = 0; i < numSelected; i++) {
-                console.log(result.response)
-                console.log(result.response.docs[i]);
-                $('#articleResults').append('title: ' + result.response.docs[i].snippet);
-                console.log(results.response.doc[i].snippet)
-                $('#articleResults').append('url: ' + results.response.docs[i].web_url);
-                console.log(results.response.docs[i].web_url)
-            }
+    }).then(function (result) {
 
-        })
-        //clearResults
-        //searchTerm
-        //numRecords
-        //for loop number placeholder number = numSelected
-        //startYear
-        //endYear
-        //search buttpn = search
-        //clear
-        //articleResults
-        console.log(queryURL)
+        for (var i = 0; i < parseInt(numSelected); i++) {
+            console.log(result.response)
+            console.log(result.response.docs[i]);
+            $('#articleResults').append('title: ' + result.response.docs[i].snippet);
+            console.log(results.response.doc[i].snippet)
+            $('#articleResults').append('url: ' + results.response.docs[i].web_url);
+            console.log(results.response.docs[i].web_url);
+        };
+
+    });
+
+    //clearResults
+    //searchTerm
+    //numRecords
+    //for loop number placeholder number = numSelected
+    //startYear
+    //endYear
+    //search buttpn = search
+    //clear
+    //articleResults
+    console.log(queryURL);
+})
