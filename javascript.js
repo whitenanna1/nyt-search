56c7f347d7c059d30b227d49cb10131f61292d57
KHL562 <khl562sk@gmail.com>
2018-12-06 20:58:04 -0800

    inital commit

---
+++ javascript.js
@@ -0,0 +1,24 @@
+        $('search').on('click', function() {
+
+        var search = $('#searchTerm').val();
+        var searchTerm = 'george bush'
+        var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=83683e6f21874fe28ac4855fd25e2dbc&q=' + searchTerm + '&apikey=83683e6f21874fe28ac4855fd25e2dbc'
+
+        $.ajax({
+        url: queryURL,
+        method: 'GET',
+        }).done(function(result) {
+        console.log(result);
+        }).fail(function(err) {
+        throw err;
+        }).then(function(response) {
+            console.log(result.doc[i])
+            for (var i = 0; i < 6; i++) {
+                
+            }
+
+        })
+
+        
+        })
+        console.log(queryURL);
