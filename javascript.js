410e85dfd4c24eff362e5d53ce9b1ba6a20e5b61
KHL562 <khl562sk@gmail.com>
2018-12-06 21:19:39 -0800

    rnd3

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
         // $('search').on('click', function() {
 
-        // var search = $('#searchTerm').val();
+        // var search = $('#search').val();
         var searchTerm = 'george bush'
         var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=83683e6f21874fe28ac4855fd25e2dbc&q=' + searchTerm + '&apikey=83683e6f21874fe28ac4855fd25e2dbc'
 
@@ -13,13 +13,25 @@
         throw err;
         }).then(function(result) {
             
-            for (var i = 0; i < 6; i++) {
-                console.log(result.response);
-                console.log(result.response.docs[1]);
-            }
+            for (var i = 0; i < numSelected; i++) {
+                console.log(result.response)
+                console.log(result.response.docs[i]);
+                $('#articleResults').append('title: ' + result.response.docs[i].snippet);
+                console.log(results.response.doc[i].snippet)
+
+            
 
         })
 
         
         // })
+        //clearResults
+        //searchTerm
+        //numRecords
+        //for loop number placeholder number = numSelected
+        //startYear
+        //endYear
+        //search buttpn = search
+        //clear
+        //articleResults
         console.log(queryURL);
